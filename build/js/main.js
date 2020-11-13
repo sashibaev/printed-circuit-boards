79bf0e4ddd31a47d8806e1b1b165037adfcc523b
Sergey Shibaev <srgjjshibaev@rambler.ru>
2020-11-13 11:00:20 +0300

    Верстает форму и модальную форму

--- build/js/main.js
+++ build/js/main.js
@@ -1,12 +1,95 @@
 'use strict';
 
 window.main = (function () {
+  var KEY_ENTER = 'Enter';
+  var KEY_ESC = 'Escape';
+  var MAIN_MOUSE_BUTTON = 1;
+
   var buttonNav = document.querySelector('.page-footer__nav-button');
   var buttonContacts = document.querySelector('.page-footer__contacts-button');
   var navList = document.querySelector('.page-footer__nav-wrapper');
   var contactsList = document.querySelector('.page-footer__contacts-list');
   var scrollLink = document.querySelector('.home-screen__scroll-link');
+  var consultationMain = document.querySelector('.home-screen__link-main');
+  var consultationMobile = document.querySelector('.home-screen__link-additional');
   var anchorAdvantages = document.querySelector('#advantages');
+  var anchorForm = document.querySelector('#form');
+  var formPopup = document.querySelector('.form-popup');
+  var closePopup = formPopup.querySelector('.form-popup__close');
+  var openPopup = document.querySelector('.page-header__feedback');
+  var sendDataFormPopup = formPopup.querySelector('.form-popup__submit');
+  var valueNameFormPopup = formPopup.querySelector('#popup-name');
+  var valueTelFormPopup = formPopup.querySelector('#popup-tel');
+  var valueCommentsFormPopup = formPopup.querySelector('#popup-comments');
+  var form = document.querySelector('.feedback__form');
+  var sendDataForm = form.querySelector('.feedback__submit');
+  var valueNameForm = formPopup.querySelector('#name');
+  var valueTelForm = formPopup.querySelector('#tel');
+  var valueCommentsForm = formPopup.querySelector('#comments');
+
+  var onPopupEscPress = function (evt) {
+    if (evt.key === KEY_ESC) {
+      closeFormPopup();
+    }
+  };
+
+  var onPopupOverlayPress = function (evt) {
+    if (evt.which === MAIN_MOUSE_BUTTON && !(evt.target).closest('.form-popup')) {
+      closeFormPopup();
+    }
+  };
+
+  var onOpenFormPopup = function () {
+    formPopup.classList.remove('hidden');
+
+    document.addEventListener('mousedown', onPopupOverlayPress);
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closeFormPopup = function () {
+    formPopup.classList.add('hidden');
+
+    document.removeEventListener('mousedown', onPopupOverlayPress);
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
+
+  openPopup.addEventListener('click', function () {
+    onOpenFormPopup();
+  });
+
+  openPopup.addEventListener('keydown', function (evt) {
+    if (evt.key === KEY_ENTER) {
+      onOpenFormPopup();
+    }
+  });
+
+  closePopup.addEventListener('click', function () {
+    closeFormPopup();
+  });
+
+  var onSendData = function (element1, element2, element3) {
+    localStorage.setItem('name', element1.value);
+    localStorage.setItem('tel', element2.value);
+    localStorage.setItem('comments', element3.value);
+  };
+
+  formPopup.addEventListener('submit', function (evt) {
+    if (evt.target !== sendDataFormPopup) {
+      onSendData(valueNameFormPopup, valueTelFormPopup, valueCommentsFormPopup);
+    }
+
+    evt.preventDefault();
+    closeFormPopup();
+  });
+
+  form.addEventListener('submit', function (evt) {
+    if (evt.target !== sendDataForm) {
+      onSendData(valueNameForm, valueTelForm, valueCommentsForm);
+    }
+
+    evt.preventDefault();
+    closeFormPopup();
+  });
 
   if (buttonNav) {
     buttonNav.classList.add('page-footer__button-menu');
@@ -34,14 +117,27 @@ window.main = (function () {
     contactsList.classList.toggle('page-footer__accordion');
   });
 
-  scrollLink.addEventListener('click', function (evt) {
-    evt.preventDefault();
-
-    if (anchorAdvantages) {
-      anchorAdvantages.scrollIntoView({
+  var onMakeMove = function (element) {
+    if (element) {
+      element.scrollIntoView({
         behavior: 'smooth',
         block: 'start'
       });
     }
+  };
+
+  scrollLink.addEventListener('click', function (evt) {
+    onMakeMove(anchorAdvantages);
+    evt.preventDefault();
+  });
+
+  consultationMain.addEventListener('click', function (evt) {
+    onMakeMove(anchorForm);
+    evt.preventDefault();
+  });
+
+  consultationMobile.addEventListener('click', function (evt) {
+    onMakeMove(anchorForm);
+    evt.preventDefault();
   });
 })();
