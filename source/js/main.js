9ee2a5a2f4525453bc816a87576d03f2adca9199
Sergey Shibaev <srgjjshibaev@rambler.ru>
2020-11-18 17:39:55 +0300

    Исправляет замечания наставника

--- source/js/main.js
+++ source/js/main.js
@@ -157,3 +157,8 @@ window.main = (function () {
     telForm: telForm
   };
 })();
+
+var Inputmask;
+var im = new Inputmask('+7([999\\)9999999]');
+im.mask(window.main.telFormPopup);
+im.mask(window.main.telForm);
--- source/js/mask-tel.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var Inputmask;
-var im = new Inputmask('+7([999\\)9999999]');
-im.mask(window.main.telFormPopup);
-im.mask(window.main.telForm);
